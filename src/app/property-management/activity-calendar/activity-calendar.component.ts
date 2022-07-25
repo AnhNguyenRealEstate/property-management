81d791ac831390917b630849f0a2b71fcf13127d
Tu Nguyen <nguyentrungtu1996@gmail.com>
2022-07-25 18:55:50 +0700

    feat(activity-calendar): add activity calendar view

--- src/app/property-management/activity-calendar/activity-calendar.component.ts
+++ src/app/property-management/activity-calendar/activity-calendar.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CalendarView } from 'angular-calendar';
 import { CalendarEvent } from 'calendar-utils'
+import { isSameDay, isSameMonth } from 'date-fns';
 import { Activity } from '../property-management.data';
 
 @Component({
@@ -11,23 +13,48 @@ export class ActivitiyCalendarComponent implements OnInit {
     @Input() activities: Activity[] = [];
     calendarEvents: CalendarEvent<Activity>[] = [];
     viewDate!: Date;
+    view: CalendarView = CalendarView.Month;
+    showEventsFromDay = false;
 
-    constructor() { }
+    constructor(
+    ) { }
 
     ngOnInit() {
         if (!this.activities.length) {
             return;
         }
-        
-        this.viewDate = this.activities[0].date!.toDate();
+
+        this.viewDate = new Date();
 
         this.calendarEvents = this.activities.map<CalendarEvent>(activity => {
             return {
                 start: activity.date?.toDate(),
-                title: activity.description,
+                title: `${activity.propertyName} - ${activity.description}`,
                 meta: activity
             } as CalendarEvent
         });
+    }
+
+    dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+        if (isSameMonth(date, this.viewDate)) {
+            if (
+                (isSameDay(this.viewDate, date) && this.showEventsFromDay === true) ||
+                events.length === 0
+            ) {
+                this.showEventsFromDay = false;
+            } else {
+                this.showEventsFromDay = true;
+            }
+            this.viewDate = date;
+        }
+    }
+
+    getPreviousMonthActivities() {
+        // TODO
+    }
+
+    getNextMonthActivities() {
+        // TODO
     }
 
 }
